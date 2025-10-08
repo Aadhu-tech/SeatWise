fc5b5339155aca575bd731521ebf649fdcd0a00d
Athul Ajai <athulajai88@gmail.com>
2025-10-09 00:59:05 +0530

    initial commit

---
+++ seatwise-code/src/gui/Main.java
@@ -0,0 +1,11 @@
+package gui;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new UserLoginFrame().setVisible(true);
+        });
+    }
+}
